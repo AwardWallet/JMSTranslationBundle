7eb0275dac6186dc1a859dd0c89b61c5cd3da4f9
Vasiliy Kurdin <vkurdin@awardwallet.com>
2021-03-23 10:44:44 +0500

    refs #20154, temporary set old path logic

--- Model/FileSource.php
+++ Model/FileSource.php
@@ -44,6 +44,9 @@ class FileSource implements SourceInterface
      */
     public function __construct($path, $line = null, $column = null)
     {
+        $parts = explode('/', str_replace(DIRECTORY_SEPARATOR, '/', $path));
+        $parts = array_splice($parts, -3);
+        $path = implode('/', $parts);
         $this->path = $path;
         $this->line = $line;
         $this->column = $column;
